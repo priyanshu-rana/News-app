72168faaa7812192e9e4aeaff869c9653038585d
Priyanshu Rana <priyanshurana1234@gmail.com>
2022-05-27 22:39:25 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-black h-screen text-white">App text-white</h1>
+    </div>
+  );
+}
+
+export default App;
